c50b271bcc953bdf5f0437d6e2d8a60363603c51
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2023-02-09 14:52:33 +0100

    [FIX] Sale order:Fix the issue that appears when creating a new line in a confirmed Sale order

--- models/sale_order_line.py
+++ models/sale_order_line.py
@@ -16,12 +16,12 @@ class SaleOrderLine(models.Model):
     @api.depends('production_request_ids')
     def _compute_qty_to_plan(self):
         for each in self:
-            # Convert the quantity to the sale order line quantity
-            if isinstance(each.production_request_ids.ids, models.NewId):
-                planned_quantity = 0.0
-            else:
+            if len(each.production_request_ids) > 1 or not isinstance(each.production_request_ids.id, models.NewId):
                 planned_quantity = sum(pr.product_uom_id._compute_quantity(pr.quantity, each.product_uom) for pr in
-                                   each.production_request_ids.filtered(lambda pr: pr.state not in ('cancel', 'draft')))
+                                       each.production_request_ids.filtered(
+                                           lambda pr: pr.state not in ('cancel', 'draft')))
+            else:
+                planned_quantity = 0.0
             each.qty_to_plan = each.product_uom_qty - planned_quantity
 
     def _can_create_production_request(self):
