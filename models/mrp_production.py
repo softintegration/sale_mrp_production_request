e219aeba6395e20fe9fd5589ebf6509c010e893a
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2024-03-04 12:19:56 +0100

    Manufacturing order:Add the field «Sequence» to table of <Sale orders> to re-order the list

--- models/mrp_production.py
+++ models/mrp_production.py
@@ -10,11 +10,31 @@ class MrpProduction(models.Model):
 
 
     sale_order_ids = fields.Many2many('sale.order',compute='_compute_sale_order_ids')
+    sale_order_line_ids = fields.One2many('mrp.production.sale.order','mrp_production_id',store=True)
 
     @api.depends('mrp_production_request_ids')
     def _compute_sale_order_ids(self):
         for each in self:
             each.sale_order_ids = each.mrp_production_request_ids.mapped("sale_id")
+            if len(each.sale_order_ids) and not each.sale_order_line_ids:
+                each.write({'sale_order_line_ids':[(0, 0, {'mrp_production_id': each.id,
+                         'sale_order_id': sale_order.id}) for sale_order in
+                 each.mrp_production_request_ids.mapped("sale_id")]})
+
+
+
+
+class MrpProductionSaleOrder(models.Model):
+    _name = 'mrp.production.sale.order'
+
+    mrp_production_id = fields.Many2one('mrp.production',string='Manufacturing',required=True)
+    sale_order_id = fields.Many2one('sale.order',string='name',required=True)
+    state = fields.Selection(related='sale_order_id.state')
+    client_order_ref = fields.Char(related='sale_order_id.client_order_ref')
+    sequence = fields.Integer(help='Used to order Sale orders', default=10,)
+
+
+
 
 
 
